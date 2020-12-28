499e113e085dbf300956d5e7df0dcf0475ef50dd
jjuny0113 <jjuny920113@gmail.com>
2020-12-28 03:38:12 -0800

    Modify: 오타 수정

--- src/navigations/Stack.js
+++ src/navigations/Stack.js
@@ -2,8 +2,9 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Tabs from "./Tabs";
 import Login from "../screens/Login";
-import SingUpInput from "../screens/SingUpInput";
-import SingUpPage from "../screens/SingUpPage";
+import SignUpInput from "../screens/SignUpInput";
+import SignUpPage from "../screens/SignUpPage";
+import CheckEmail from "../screens/CheckEmail";
 import Loading from "../screens/Loading";
 import Daily from "../screens/Daily";
 import DetailPage from "../screens/DetailPage";
@@ -15,8 +16,9 @@ export default () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
     <Stack.Screen name="Tabs" component={Tabs} />
     <Stack.Screen name="Login" component={Login} />
-    <Stack.Screen name="SingUpInput" component={SingUpInput} />
-    <Stack.Screen name="SingUpPage" component={SingUpPage} />
+    <Stack.Screen name="SignUpInput" component={SignUpInput} />
+    <Stack.Screen name="SignUpPage" component={SignUpPage} />
+    <Stack.Screen name="CheckEmail" component={CheckEmail} />
     <Stack.Screen name="Loading" component={Loading} />
     <Stack.Screen name="Daily" component={Daily} />
     <Stack.Screen name="LocalTrip" component={LocalTrip} />
