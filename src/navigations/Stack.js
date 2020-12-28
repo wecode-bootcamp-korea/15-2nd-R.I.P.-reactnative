77e2be2c4221c3897b243c6cb03eb701a91ff401
jjuny0113 <jjuny920113@gmail.com>
2020-12-28 03:35:16 -0800

    Add:Stack login, singup screen 추가

--- src/navigations/Stack.js
+++ src/navigations/Stack.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Tabs from "./Tabs";
+import Login from "../screens/Login";
+import SingUpInput from "../screens/SingUpInput";
+import SingUpPage from "../screens/SingUpPage";
+import Loading from "../screens/Loading";
 import Daily from "../screens/Daily";
 import DetailPage from "../screens/DetailPage";
 import LocalTrip from "../screens/LocalTrip";
-import Loading from "../screens/Loading";
 
 const Stack = createStackNavigator();
 
 export default () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
     <Stack.Screen name="Tabs" component={Tabs} />
+    <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="SingUpInput" component={SingUpInput} />
+    <Stack.Screen name="SingUpPage" component={SingUpPage} />
     <Stack.Screen name="Loading" component={Loading} />
     <Stack.Screen name="Daily" component={Daily} />
     <Stack.Screen name="LocalTrip" component={LocalTrip} />
